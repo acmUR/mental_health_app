dcb83e2458e91eec5fcfee91104761091a8b3dde
Nikita Morozov <okay.nik2@gmail.com>
2022-10-15 16:31:36 -0400

    changed color of the button

--- lib/main.dart
+++ lib/main.dart
@@ -119,8 +119,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
+        backgroundColor: Colors.red,
         child: const Icon(Icons.add),
-        backgroundColor: Colors.cyanAccent,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
