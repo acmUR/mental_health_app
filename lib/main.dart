077d69b99095ef9a1c7e04e1f02625936aeb4611
Liz Smith <liz.smith@richmond.edu>
2023-01-28 18:01:00 -0500

    umm

--- lib/main.dart
+++ lib/main.dart
@@ -14,69 +14,57 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const InfoHomePage(title: 'Information'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class InfoHomePage extends StatefulWidget {
+  const InfoHomePage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<InfoHomePage> createState() => _InfoHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  List<int> arr = [];
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _InfoHomePageState extends State<InfoHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // return Person(age: 10, name: John)
-
     return Scaffold(
         appBar: AppBar(
-      // Here we take the value from the MyHomePage object that was created by
-      // the App.build method, and use it to set our appbar title.
-      title: Text(widget.title),
-    ));
+          // Here we take the value from the InfoHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Text(
+              'All UR\'s information for YOUR needs in ONE place.',
+              textAlign: TextAlign.center,
+            ),
+            Text('(you\'re welcome)',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontStyle: FontStyle.italic, fontSize: 10)),
+            ElevatedButton(
+              onPressed: null,
+              child: const Text('Emergency'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: const Text('Social'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: const Text('Academic'),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
+        ));
   }
 }
